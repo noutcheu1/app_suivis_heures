a1e8c1a72571ddcfbcb3d2486d460203c0daac79
noutcheu libert <libert.noutcheu@gmail.com>
2026-05-12 17:10:30 +0200

    test

--- src/Security/Auth.php
+++ src/Security/Auth.php
@@ -1,42 +1,54 @@
 <?php
+
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class Auth
 {
-    public function __construct(private SessionInterface $session) {}
+    public function __construct(
+        private RequestStack $requestStack
+    ) {}
+
+    private function session()
+    {
+        return $this->requestStack->getSession();
+    }
 
     public function check(): bool
     {
-        return $this->session->has('username');
+        return $this->session()->has('username');
     }
 
-    public function is_intervenant() : bool
+    public function is_intervenant(): bool
     {
-        return $this->session->has('intervenant_id') && $this->session->has('candidat_id');
+        return $this->session()->has('intervenant_id')
+            && $this->session()->has('candidat_id');
     }
 
-    public function is_famille() : bool {
-        return $this->session->has('famille_id'); 
-    }
-
-    public function user(): ? string
+    public function is_famille(): bool
     {
-        return $this->session->get('username');
+        return $this->session()->has('famille_id');
     }
 
-    public function intervenant_id(): ? string {
-        return $this->session->get('intervenant_id');
+    public function user(): ?string
+    {
+        return $this->session()->get('username');
     }
 
-    public function candidat_id(): ? string {
-        return $this->session->get('candidat_id');
+    public function intervenant_id(): ?string
+    {
+        return $this->session()->get('intervenant_id');
     }
 
-    public function famille_id(): ? string {
-        return $this->session->get('famille_id');
+    public function candidat_id(): ?string
+    {
+        return $this->session()->get('candidat_id');
+    }
+
+    public function famille_id(): ?string
+    {
+        return $this->session()->get('famille_id');
     }
 
     public function isAdmin(): bool
